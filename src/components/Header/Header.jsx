717037199b1b14e4225d3d1b52fd7d93493fe2bf
dacentra <fahad@dacentra.com>
2025-05-03 17:53:32 +0800

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -117,22 +117,22 @@ const Header = ({ className }) => {
                                     <Link>Become a Partner<span>Grow your business and revenue</span></Link>
                                 </li>
                                 <li>
-                                    <Link to='/benefits'>Benefits<span>See the advantages of using Escrow</span></Link>
+                                    <Link to='/benefits'>Benefits<span>See the advantages of using Escrowly</span></Link>
                                 </li>
                                 <li>
                                     <Link to="/fee-calculator">Fees<span>Low transparent fees</span></Link>
                                 </li>
                                 <li>
-                                    <Link to="/escrowly-pay">Escrow Pay<span>Simplest way to add escrow payments</span></Link>
+                                    <Link to="/escrowly-pay">Escrowly Pay<span>Simplest way to add escrowly payments</span></Link>
                                 </li>
                                 <li>
-                                    <Link to="/escrowly-offer">Escrow Offer<span>Allow negotiation for your transactions</span></Link>
+                                    <Link to="/escrowly-offer">Escrowly Offer<span>Allow negotiation for your transactions</span></Link>
                                 </li>
                                 <li>
-                                    <Link to="/escrowly-buttons">Escrow Buttons<span>Create a button for Escrow transaction</span></Link>
+                                    <Link to="/escrowly-buttons">Escrowly Buttons<span>Create a button for Escrowly transaction</span></Link>
                                 </li>
                                 <li>
-                                    <Link to="/api-guide">Escrow API<span>Get all benefits of Escrow via API</span></Link>
+                                    <Link to="/api-guide">Escrowly API<span>Get all benefits of Escrowly via API</span></Link>
                                 </li>
                                 <li className="bordered">
                                     <Link>Start A Transaction With Escrowly<span>Sell, buy or broker anything from domain names to real estate</span></Link>
@@ -174,7 +174,7 @@ const Header = ({ className }) => {
                                     <Link to="/our-partners">Our Partners <span>Escrowly partners with world leaders</span></Link>
                                 </li>
                                 <li>
-                                    <Link to="/partners-enquiry">Partners Inquiry <span>For partnership inquiries contact sales</span></Link>
+                                    <Link to="/partners-enquiry">Partners Inquiry <span>For partnership inquiries contact us</span></Link>
                                 </li>
                                 <li className="bordered">
                                     <Link>Start A Transaction With Escrowly<span>Sell, buy or broker anything from domain names to real estate</span></Link>
@@ -186,7 +186,7 @@ const Header = ({ className }) => {
                             <Link to="/about-us">Help</Link>
                             <ul className="sub-menu">
                                 <li>
-                                    <Link to="/what-is-escrowly">What is Escrowly? <span>Learn how Escrow works</span></Link>
+                                    <Link to="/what-is-escrowly">What is Escrowly? <span>Learn how crypto escrow works</span></Link>
                                 </li>
                                 <li>
                                     <Link to="/fee-calculator">Fees<span>Low transparent fees</span></Link>
@@ -195,7 +195,7 @@ const Header = ({ className }) => {
                                     <Link to="/help-desk">Contact Us<span>Get in touch with us</span></Link>
                                 </li>
                                 <li>
-                                    <Link>Call Us<span>+1-415-801-2270</span></Link>
+                                    <Link>Call Us<span>+1-302-444-8419</span></Link>
                                 </li>
                                 <li>
                                     <Link to="/help-desk">Help Desk<span>Find answers to FAQs</span></Link>
